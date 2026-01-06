b474e51859676bef1966f81c0b536b008c4fe76c
Priyanshu Kumar <priyanshukumarpriyanshu015@gmail.com>
2026-01-06 18:51:49 +0530

    Update CORS origin to new render URL

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://ecosense-4.onrender.com",
+      "https://ecosense-7.onrender.com",
     ],
     credentials: true,
   })
